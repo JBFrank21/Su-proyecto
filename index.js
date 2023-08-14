51d69256ad41e4bc24e8c404a82d3c77a6ee0e7b
JBFrank21 <140364806+JBFrank21@users.noreply.github.com>
2023-08-13 20:35:30 -0500

    d

---
+++ index.js
@@ -0,0 +1,20 @@
+let unoBtn = document.getElementById("uno");
+let dosBtn = document.getElementById("dos");
+let tresBtn = document.getElementById("tres");
+let cuatroBtn = document.getElementById("cuatro");
+
+unoBtn.onclick=function(){
+    tres.src="https://images.cdn1.buscalibre.com/fit-in/360x360/6e/8c/6e8c464ed8ff22c7b8ee34d19f76aa6c.jpg";
+}
+
+dosBtn.onclick=function(){
+    cuatro.src="https://images.cdn1.buscalibre.com/fit-in/360x360/bf/38/bf388c018a02453b752bc1cacd57a067.jpg";
+}
+
+tresBtn.onclick=function(){
+    uno.src="https://images.cdn3.buscalibre.com/fit-in/360x360/60/f1/60f1f81bd7586d65451c4551311c4979.jpg";
+}
+
+cuatroBtn.onclick=function(){
+    dos.src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQtIgatTNa8_xY8C3aeGu2Bh2TpHHMhr2Z2GMMMFfRQCoWNb7W1";
+}
